d92312cfb6da311b3f70c407f7ca4571a7b8a61b
Moonyll <s.pouwel@laposte.net>
2018-12-17 17:15:06 +0100

    Ex8

---
+++ Ex8.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rayon du cercle ?");
+            int r = int.Parse(Console.ReadLine());
+            Double perimeter = 2 * Math.PI * r;
+            Double surface = r * r * Math.PI;
+            Console.WriteLine("Perimeter is = " + perimeter);
+            Console.WriteLine("Surface is = " + surface);
+
+
+        }
+    }
+}
